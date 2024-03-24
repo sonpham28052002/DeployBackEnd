b772a8311441257e1882ba39092ddda4f1493c82
sonpham28052002 <sonpham28052002@gmail.com>
2024-03-25 03:52:06 +0700

    config cors

--- src/main/java/vn/edu/iuh/fit/chat_backend/ChatBackEndApplication.java
+++ src/main/java/vn/edu/iuh/fit/chat_backend/ChatBackEndApplication.java
@@ -17,7 +17,7 @@ public class ChatBackEndApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
 			}
 		};
 	}
