4cd65ce007865be4c90345259bacc17dbf8eadd8
sonpham28052002 <sonpham28052002@gmail.com>
2024-03-24 16:47:08 +0700

    change handle cloud azure

---
+++ src/main/java/vn/edu/iuh/fit/chat_backend/controllers/AzureController.java
@@ -0,0 +1,26 @@
+package vn.edu.iuh.fit.chat_backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import vn.edu.iuh.fit.chat_backend.services.AzureStorageService;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("azure")
+public class AzureController {
+    @Autowired
+    private AzureStorageService azureStorageServive;
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam MultipartFile file) throws IOException {
+        String type =file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+        return azureStorageServive.uploadFile(UUID.randomUUID().toString()+"."+type,file.getInputStream(),file.getSize(),file.getContentType());
+    }
+    @GetMapping("/file")
+    public String string(){
+        return "phạm thanh son";
+    }
+}
---
+++ src/main/java/vn/edu/iuh/fit/chat_backend/services/AzureStorageService.java
@@ -0,0 +1,41 @@
+package vn.edu.iuh.fit.chat_backend.services;
+
+
+import com.azure.core.util.Context;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobHttpHeaders;
+import com.azure.storage.blob.options.BlobParallelUploadOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+
+@Service
+public class AzureStorageService {
+    @Value("${azure.connection}")
+    private String connection;
+
+
+    @Value("${azure.container}")
+    private String container;
+
+    private BlobContainerClient containerClient() {
+        System.out.println(container);
+        BlobServiceClient serviceClient = new BlobServiceClientBuilder()
+                .connectionString(connection).buildClient();
+        return serviceClient.getBlobContainerClient(container);
+    }
+
+    public String uploadFile(String fileName, InputStream inputStream, long length, String type) {
+        BlobClient client = containerClient().getBlobClient(fileName);
+        BlobHttpHeaders headers = new BlobHttpHeaders().setContentType(type);
+        BlobParallelUploadOptions options = new BlobParallelUploadOptions(inputStream,length)
+                .setHeaders(headers);
+        client.uploadWithResponse(options, null, Context.NONE);
+        return client.getBlobUrl();
+    }
+
+}
