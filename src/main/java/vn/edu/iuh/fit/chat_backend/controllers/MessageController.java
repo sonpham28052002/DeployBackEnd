f9008b245dedad5afc6669ab1f5f9e33bf899584
sonpham28052002 <sonpham28052002@gmail.com>
2024-04-03 12:05:29 +0700

    fix merge

--- src/main/java/vn/edu/iuh/fit/chat_backend/controllers/MessageController.java
+++ src/main/java/vn/edu/iuh/fit/chat_backend/controllers/MessageController.java
@@ -1,6 +1,5 @@
 package vn.edu.iuh.fit.chat_backend.controllers;
 
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +11,6 @@ import vn.edu.iuh.fit.chat_backend.models.ConversationSingle;
 import vn.edu.iuh.fit.chat_backend.models.Message;
 import vn.edu.iuh.fit.chat_backend.models.User;
 import vn.edu.iuh.fit.chat_backend.repositories.UserRepository;
-=======
-import net.datafaker.Faker;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import vn.edu.iuh.fit.chat_backend.dtos.MessageDto;
-import vn.edu.iuh.fit.chat_backend.models.Message;
-import vn.edu.iuh.fit.chat_backend.models.MessageFile;
-import vn.edu.iuh.fit.chat_backend.models.MessageText;
-import vn.edu.iuh.fit.chat_backend.models.User;
-import vn.edu.iuh.fit.chat_backend.repositories.MessageRepository;
-import vn.edu.iuh.fit.chat_backend.repositories.UserRepository;
-import vn.edu.iuh.fit.chat_backend.types.MessageType;
->>>>>>> origin/main
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -34,12 +18,8 @@ import java.util.Optional;
 @RestController
 @RequestMapping(value = "/messages", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MessageController {
-<<<<<<< HEAD
-
     @Autowired
     private UserRepository userRepository;
-
-
 
     @GetMapping("/getMessageByIdSenderAndIsReceiver")
     public List<Message> getMessageByIdSenderAndIsReceiver(@RequestParam String idSender, @RequestParam String idReceiver) {
@@ -48,7 +28,7 @@ public class MessageController {
         Optional<User> receiver = userRepository.findById(idReceiver);
         System.out.println(receiver);
 
-        if (sender.isEmpty() || receiver.isEmpty()){
+        if (sender.isEmpty() || receiver.isEmpty()) {
             return new ArrayList<>();
         }
         for (Conversation conversation : sender.get().getConversation()) {
@@ -59,55 +39,6 @@ public class MessageController {
             }
         }
         return new ArrayList<>();
-=======
-    @Autowired
-    private MessageRepository messageRepository;
-    @Autowired
-    private UserRepository userRepository;
-
-    @GetMapping("/all")
-    public List<Message> getAll() {
-        return messageRepository.findAll();
     }
 
-    @GetMapping("/getMessageById")
-    public Optional<Message> getMessageById(@RequestParam String id) {
-
-        Optional<Message> message = messageRepository.findById(id);
-
-        return message;
-    }
-
-    @GetMapping("/getMessageBySender")
-    public List<Message> getMessageBySender(@RequestParam String id) {
-
-        List<Message> messagesBySender = messageRepository.findAllBySender(userRepository.findById(id).get());
-
-        return messagesBySender;
-    }
-
-    @PostMapping("/insertMessage")
-    public Message insertUser(@RequestBody MessageDto dto) {
-        User sender = userRepository.findById(dto.getSender().getId()).get();
-        User receiver = userRepository.findById(dto.getReceiver().getId()).get();
-        if(dto.getContent() != null){ // text
-            MessageText messageText = new MessageText();
-            messageText.setId(dto.getId());
-            messageText.setMessageType(dto.getMessageType());
-            messageText.setSender(sender);
-            messageText.setReceiver(receiver);
-            messageText.setContent(dto.getContent());
-            return messageRepository.save(messageText);
-        }
-        MessageFile messageFile = new MessageFile();
-        messageFile.setId(dto.getId());
-        messageFile.setMessageType(dto.getMessageType());
-        messageFile.setSender(sender);
-        messageFile.setReceiver(receiver);
-        messageFile.setSize(dto.getSize());
-        messageFile.setTitleFile(dto.getTitleFile());
-        messageFile.setUrl(dto.getUrl());
-        return messageRepository.save(messageFile);
->>>>>>> origin/main
-    }
 }
