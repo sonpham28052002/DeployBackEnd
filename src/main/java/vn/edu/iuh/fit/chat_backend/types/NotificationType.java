b628ebbc255d811045b82cc29a5a1aed182bbc93
sonpham28052002 <sonpham28052002@gmail.com>
2024-05-15 06:58:11 +0700

    handle call group

--- src/main/java/vn/edu/iuh/fit/chat_backend/types/NotificationType.java
+++ src/main/java/vn/edu/iuh/fit/chat_backend/types/NotificationType.java
@@ -7,7 +7,7 @@ public enum NotificationType {
     CREATE_GROUP("CREATE_GROUP"),
     JOIN_CALL("JOIN_CALL"),
     ADD_MEMBER("ADD_MEMBER"),
-    OUT_GROUP("OUT_GROUP"),
+    OUT_CALL("OUT_CALL"),
     GET_OUT_GROUP("GET_OUT_GROUP");
 
     private final String notify;
